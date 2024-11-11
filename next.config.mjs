11a35cab8eab17a8c44e6b6eeb7db0da6c475373
joshikannan <joshikannan99@gmail.com>
2024-11-11 10:14:05 +0530

    nextConfig edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone", // Optional: useful for custom server setups
+};
 
 export default nextConfig;
