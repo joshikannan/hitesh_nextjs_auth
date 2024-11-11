31c59759ac15ed1ad364c805ba7dce58474b2686
joshikannan <joshikannan99@gmail.com>
2024-11-11 11:30:10 +0530

    middleware removed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Stack } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Welcome to home page</h1>
+      <Stack spacing={2}>
+        <h1>Welcome to home page</h1>
+        <Link href={"/signup"}> Signup</Link>
+        <Link href={"/login"}> Login</Link>
+      </Stack>
     </div>
   );
 }
